67b96c31a06c621dd37aab3b138269b3f7d36651
GhostmaneX <fes1ytxd@gmail.com>
2023-10-30 15:34:22 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,3 +1,11 @@
 Config = {}
 
-Config.Reward = 500
+Config.CoreObject = "QBCore"
+
+Config.Settings = {
+    CoreExport = "qb-core",
+    TargetExport = "qb-target",
+    DispatchExport = "qb-dispatch",
+    UseageCooldown = 30000,
+    Payment = "cash"
+}
