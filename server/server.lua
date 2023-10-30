69da4d0513d49702bd525ea85c7b5cbf7e7e546a
GhostmaneX <fes1ytxd@gmail.com>
2023-10-30 15:34:08 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,8 +1,19 @@
-local QBCore = exports['qb-core']:GetCoreObject()
+local CoreObject = exports[Config.Settings.CoreExport]:GetCoreObject()
 
-RegisterNetEvent('apachivending:AddMoney', function()
+RegisterNetEvent('apachi-vendingrobbery:RobberyReward', function()
     local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    Player.Functions.AddMoney('cash', Config.Reward, 'Vending Robbery')
-    
+    local price = math.random(300, 600)
+    local Player = CoreObject.Functions.GetPlayer(src)
+    Player.Functions.AddMoney(Config.Settings.Payment, price, "sold-casino-chips")
+end)
+
+CoreObject.Functions.CreateCallback('apachi-vendingrobbery:CheckForItems', function(source, cb)
+    local src = source
+    local Ply = CoreObject.Functions.GetPlayer(src)
+    local hammer = Ply.Functions.GetItemByName("weapon_hammer")
+    if hammer ~= nil then
+        cb(true)
+    else
+        cb(false)
+    end
 end)
