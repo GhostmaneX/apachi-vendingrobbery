d00cac6d74c20f195c01236cb734c0a038eac68e
GhostmaneX <fes1ytxd@gmail.com>
2023-10-30 15:34:36 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,14 +1,28 @@
-fx_version 'cerulean'
-game 'gta5'
-lua54 'yes'
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
 
-description 'Apachi shop vending robbery'
-author 'Apachi Shop'
-version '1.2'
+description "Apachi shop vending robbery, upgraded by GhostmaneX"
+author "Apachi Shop"
+version "1.3"
 
-server_script 'server/*.lua'
-client_script 'client/*.lua'
+ui_page "html/index.html"
+
+server_scripts {
+	"server/*.lua"
+}
+
+client_scripts {
+	"client/*.lua"
+}
 
 shared_scripts {
-	'config.lua'
+	"config.lua",
+	"locales/en.lua"
+}
+
+files {
+	"html/js/*.js",
+	"html/index.html",
+	"html/style.css",
 }
